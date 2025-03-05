fd793cfd3b6e5e63b2cff294f4809e83cb797dd9
Ian Baquero <ianbd_96@hotmail.com>
2025-03-05 00:57:17 -0500

    Updated: 05_Open_Weather_Maps

--- 05_Open_Weather_Maps/index.js
+++ 05_Open_Weather_Maps/index.js
@@ -20,20 +20,30 @@ const main = async () => {
 
                 // Seleccionar el lugar
                 const id = await listarLugares(lugares);
+                if (id === '0') continue;
                 const lugarSelec = lugares.find(l => l.id === id);
-                // Clima
 
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSelec.nombre);
+                // Clima
+                const clima = await busquedas.climaLugar(lugarSelec.lat, lugarSelec.lng)
                 // Mostrar Resultados
                 console.log('\nInformacion de la ciudad\n'.green);
-                console.log('Ciudad:',lugarSelec.nombre);
-                console.log('Lat:',lugarSelec.lat);
-                console.log('Lng:',lugarSelec.lng);
-                console.log('Temperature: ');
-                console.log('Minima: ');
-                console.log('Maxima: ');
+                console.log('Ciudad:', lugarSelec.nombre);
+                console.log('Lat:', lugarSelec.lat);
+                console.log('Lng:', lugarSelec.lng);
+                console.log('Temperature: ', clima.temperature);
+                console.log('Minima: ', clima.minTemp);
+                console.log('Maxima: ', clima.maxTemp);
+                console.log('Descripcion: ', clima.description.green);
 
-                break;
-
+            break;
+            case '2':
+                busquedas.hitorialCapitalizado.forEach((lugar, i) => {
+                    const idx = `${i + 1}.`.green;
+                    console.log(`${idx} ${lugar}`);
+                })
+            break;
         }
         if (opt !== '0') await pausa();
         // const texto = await leerInput('Hola: ');
--- 05_Open_Weather_Maps/models/busqueda.js
+++ 05_Open_Weather_Maps/models/busqueda.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Busquedas {
-    historial = ['Madrid', 'Quito', 'Bogota'];
+    historial = [];
+    dbPath = './db/database.json';
     constructor() {
+        this.readDB();
+    }
 
+    get hitorialCapitalizado(){
+        // Capitalizar cada palabra
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ');
+        });
     }
 
     get paramsMapbox() {
@@ -11,6 +23,14 @@ class Busquedas {
             'access_token': process.env.MAPBOX_KEY,
             'limit': 5,
             'language': 'en'
+        }
+    }
+
+    get paramsWeatherAPI(){
+        return{
+            'appid': process.env.OPENWEATHER_KEY,
+            'units': 'metric',
+            'lang': 'es'
         }
     }
 
@@ -37,6 +57,71 @@ class Busquedas {
         }
     }
 
+    async climaLugar(lat,lng){
+        try {
+            // instance axios.create()
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}`,
+                params: this.paramsWeatherAPI
+                // este es otro modo de enviar la lat y long con desestructuracion en caso que no queramos poner las variables 
+                // lat y long en el baseURL:
+                // params: {...this.paramsWeatherAPI, lat, lng}  // OJO el baseURL seria:`https://api.openweathermap.org/data/2.5/weather` 
+            });
+            // resp.data
+            const resp = await instance.get();
+            // const clima = resp.data;
+            // const resultado = {
+            //     id: clima.weather[0].id,
+            //     temperature: clima.main.temp,
+            //     minTemp: clima.main.temp_min,
+            //     maxTemp: clima.main.temp_max,
+            //     description: clima.weather[0].description,
+            // };
+            // usando desestructuracion de datos tambien se puede obtener las variables
+            const {weather, main} = resp.data;
+            return {
+                description:weather[0].description,
+                minTemp: main.temp_min,
+                maxTemp: main.temp_max,
+                temperature: main.temp
+            }
+            //return resultado;
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    agregarHistorial (lugar = ''){
+        // TODO: Prevenir duplicidad
+        if(this.historial.includes(lugar.toLocaleLowerCase())){
+            return;
+        }
+        // este pedaso de codigo solo mantiene 6 items en el historial
+        this.historial = this.historial.splice(0,5);
+
+        this.historial.unshift(lugar.toLocaleLowerCase());
+        this.saveDB();
+    }
+
+    saveDB(){
+
+        const payload = {
+            historial: this.historial
+        };
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+    readDB(){
+        if( !fs.existsSync(this.dbPath) ){
+            return null;
+        }
+        
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8' });
+        const data = JSON.parse( info );
+        this.historial = data.historial;
+        
+    }
 }
 
 module.exports = Busquedas;
